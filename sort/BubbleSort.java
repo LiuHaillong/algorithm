c8206be98cd6dfeba983e9ce6e61e915614c3ae3
LiuHailong <787346616@qq.com>
2015-07-14 10:15:41 +0800

    add bubblesort

---
+++ sort/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.liu.test;
+
+import java.util.Scanner;
+
+/**
+ * ц╟ещеепР
+ */
+public class BubbleSort {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		int a[] = new int[len];
+		for (int i = 0; i < len; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<len-1;i++){
+			for(int j=0;j<len-1;j++){
+				if(a[j]<a[j+1]){
+					int tmp = a[j+1];
+					a[j+1] = a[j];
+					a[j] = tmp;
+				}
+			}
+		}
+		
+		for(int i=0;i<len;i++){
+			System.out.print(a[i]+" ");
+		}
+		sc.close();
+	}
+}
