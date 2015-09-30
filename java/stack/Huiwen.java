6ed19f27d50bbf33508471be730ba5b7fb9a5bc4
LiuHailong <787346616@qq.com>
2015-09-30 13:51:31 +0800

    update Huiwen

--- java/stack/Huiwen.java
+++ java/stack/Huiwen.java
@@ -6,37 +6,36 @@ import java.util.Scanner;
  * 判断一个字符串是否是回文字符串
  */
 public class Huiwen {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		char[] a = new char[101];
-		char[] s = new char[101];
-		a = sc.next().toCharArray();
-		int i,len,mid,next,top;
-		len = a.length;
-		//计算中间点
-		mid = len/2 - 1;
-		//初始化栈
-		top = 0;
-		for(i=0;i<=mid;i++){
-			s[++top] = a[i];
-		}
-		
-		if(len%2==0)
-			next = mid + 1;
-		else
-			next = mid + 2;
-		
-		for(i=next;i<=len-1;i++){
-			if(s[top]!=a[i])
-				break;
-			top--;
-		}
-		
-		if(top==0)
-			System.out.println("YES");
-		else
-			System.out.println("NO");
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] a ;
+        char[] s = new char[101];
+        a = sc.next().toCharArray();
+        int i, len, mid, next, top;
+        len = a.length;
+        //计算中间点
+        mid = len / 2 - 1;
+        //初始化栈
+        top = 0;
+        for (i = 0; i <= mid; i++) {
+            s[++top] = a[i];
+        }
+        if (len % 2 == 0)
+            next = mid + 1;
+        else
+            next = mid + 2;
+
+        for (i = next; i <= len - 1; i++) {
+            if (s[top] != a[i])
+                break;
+            top--;
+        }
+
+        if (top == 0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+        sc.close();
+    }
 }
